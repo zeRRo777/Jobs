80f108ef3a62ec93787c80c4693318a9aaef610b
zeRRo777 <nefedova.2017@gmail.com>
2024-12-13 15:11:22 +0300

    make models: City, Company, Tag, Vacancy and make migrations for them

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Company;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profession')->nullable();
+            $table->string('photo')->nullable();
+            $table->text('resume')->nullable();
+            $table->integer('type');
+            $table->foreignIdFor(Company::class);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2024_12_13_111818_create_user_city_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use App\Models\City;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_city', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(City::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_city');
+    }
+};
