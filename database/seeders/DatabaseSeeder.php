856be3d18fb2c5873873d888fc88936442d895bc
zeRRo777 <nefedova.2017@gmail.com>
2024-12-17 16:59:02 +0300

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,7 +7,7 @@ use App\Models\Company;
 use App\Models\Tag;
 use App\Models\User;
 use App\Models\Vacancy;
-use Database\Factories\CityFactory;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -30,29 +30,42 @@ class DatabaseSeeder extends Seeder
 
         //для каждой компании 10 вакансий и 1 админ
         foreach ($companies as $company) {
+
+            //1 случайный город
+            $city = $cities->random(1)->first();
+
             //1 админ
-            User::factory()->count(1)->admin()->state(['company_id' => $company->id])->create();
+            $user = User::factory()->admin()->state(['company_id' => $company->id])->create();
+
+            //добавляем админа к компании
+            $user->cities()->attach($city);
+
+            //добавляем город к компании
+            $company->cities()->attach($city);
 
             //10 вакансий
-            $vacancies = Vacancy::factory()->count(10)->state(['company_id' => $company->id, 'city_id' => $cities->random(1)->first()->id])->create();
+            $vacancies = Vacancy::factory()->count(10)->state(['company_id' => $company->id, 'city_id' => $city->id])->create();
 
             //добавляем созданные вакансии в коллекцию
             $allVacancies = $allVacancies->merge($vacancies);
 
             //к каждой вакансии 2 тега и добавление города
             foreach ($vacancies as $vacancy) {
-                $randomtags = $tags->random(2);
-                $vacancy->tags()->attach($randomtags);
+                $vacancy->tags()->attach($tags->random(2));
             }
         }
 
         //10 обычных пользователей
         $users = User::factory()->count(10)->create();
 
+        //добавление каждому пользователю случайного города
+        foreach ($users as $user) {
+            $user->cities()->attach($cities->random(1));
+        }
+
         //каждому пользователю 3 случайные понравившиеся вакансии
         foreach ($users as $user) {
-            $randomVacancies = $allVacancies->random(3);
-            $user->likedVacancies()->attach($randomVacancies);
+            $user->likedVacancies()->attach($allVacancies->random(3));
         }
     }
 }
