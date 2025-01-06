2ed575ef2b949f243c84d4609d4718a46d75ea75
zeRRo777 <nefedova.2017@gmail.com>
2025-01-06 14:13:14 +0300

    fix migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('photo')->nullable();
             $table->text('resume')->nullable();
             $table->integer('type');
-            $table->foreignIdFor(Company::class)->nullable();
+            $table->foreignIdFor(Company::class)->nullable()->constrained()->nullOnDelete();
             $table->rememberToken();
             $table->timestamps();
         });
